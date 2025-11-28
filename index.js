b6024db51fb3dc2ac9a0a77c48529e684fd746f8
Chitranjan kumar <chitranjan75075@gmail.com>
2025-11-28 23:02:11 +0530

    Remove commented-out promise examples and add fetch calls

--- index.js
+++ index.js
@@ -1,1 +1,166 @@
+// var ans = new Promise((res,rej)=>{
+// var n = Math.floor(Math.random()*10);
+//  if(n<5){
+//    return res();
+//  }
+//  else{
+//   return rej();
+//  }
+// })
+
+// ans
+// .then(function () {
+//   console.log("above");
+  
+// })
+// .catch(function() {
+//   console.log("small");
+  
+  
+// })
+
+
+// 1 pahale utho 
+
+// 2 washroom jaav
+// 3 cooked food
+// 4go duty
+
+// var daiy = new Promise(function(res,req){
+
+//   return res("pahale utho")
+// })
+// var pro2 = daiy.then(function(data) {
+//   console.log(data);
+//   return  new Promise(function(res ,req) {
+//     return res("washroom jaav");
+    
+    
+//   })
+//   })
+//   var pro3 = pro2.then(function(data) {
+//     console.log(data);
+//    return new Promise (function(res , req) {
+//     return res("cookes food");
+    
+    
+//    }) 
+    
+//   })
+//   var pro4 = pro3.then(function (data) {
+//     console.log(data);
+//     return new Promise(function (res, rej) {
+//       return res ("go duty")
+      
+//     })
+    
+    
+//   })
+
+//  pro4.then(function(data) {
+//     console.log(data);
+    
+    
+//   })
+//   daiy.catch(function(err) {
+//     console.log("Pending");
+    
+    
+//   })
+
+  // 1 ravi first
+  // 2 sani second
+  // 3 taip  
+  // 4 win by chitranjan
+  
+//   var uno =new Promise(function (res ,req) {
+//    return res ("ravi first") 
+//   })
+//   var uno1=uno.then(function (data) {
+//     console.log(data);
+//     return new Promise(function (res ,req) {
+//       return res("sani second")
+//     })
+//   })
+
+//  var uno2 = uno1.then(function (data) {
+//     console.log(data);
+//     return new Promise(function (res , req) {
+//       return res ("trip")
+      
+//     })
+    
+//   })
+//   var uno3 = uno2.then(function (data) {
+//     console.log(data);
+//     return new Promise(function (res ,rej) {
+//       return res("win by Chitranjan")
+      
+//     })
+    
+//   })
+//   uno3 .then(function (data) {
+//     console.log(data);
+    
+    
+//   })
+
+function data1() {
+  fetch(`https://randomuser.me/api/`)
+  .then(function(raw) {
+    return raw.json();
+    
+  })
+  .then (function (data) {
+    console.log(data);
+    
+    
+  })
+  
+}
+
+data1();
+
+ async function data () {
+     let raw = fetch(`https://randomuser.me/api/`)
+       let res= await (await raw).json()
+        console.log(res);
+        
+      }
+
+  data();
+
+
+
+async function fetchData() {
+  try {
+    const response = await fetch('https://api.example.com/data'); 
+    const data = await response.json(); 
+    console.log(data);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+}
+
+fetchData();
+
+function withPromises() {
+
+  let promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Hello World, after 3 seconds");
+    }, 3000);
+  });
+
+  promise.then((data) => {
+    console.log(data);
+  });
+
+  
+  console.log("Some Synchrnous Code");
+}
+
+withPromises(); 
+
+
 
